baf6e6f4a6a1bc9fec1a01360e5a984c77dd6c39
Daniel Villegas <danielsvillegas17@gmail.com>
2026-08-18 12:33:32 -0300

    fix(dynamodb): eliminar atributos no indexados de schema

--- dynamodb_quiz_results.tf
+++ dynamodb_quiz_results.tf
@@ -2,50 +2,40 @@ resource "aws_dynamodb_table" "quiz_results" {
   name         = "${var.project_name}-QuizResults-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ResultID"
+
   attribute {
     name = "ResultID"
     type = "S"
   }
+
   attribute {
     name = "QuizID"
     type = "S"
   }
+
   attribute {
     name = "StudentID"
     type = "S"
   }
+
   attribute {
-    name = "QuestionID"
+    name = "Timestamp"
     type = "S"
   }
 
-  attribute {
-    name = "GivenAnswer"
-    type = "S"
-  }
-
-  attribute {
-    name = "IsCorrect"
-    type = "S"
-  }
-  attribute {
-  name = "Timestamp"
-  type = "S"
-}
-global_secondary_index {
-    name            = "StudentResultsIndex"
+  global_secondary_index {
+    name            = "QuizIndex"
+    hash_key        = "QuizID"
     projection_type = "ALL"
-
-    key_schema {
-      attribute_name = "StudentID"
-      key_type       = "HASH"      # Partition Key del GSI
-    }
-
-    key_schema {
-      attribute_name = "Timestamp"
-      key_type       = "RANGE"     # Sort Key del GSI
-    }
   }
+
+  global_secondary_index {
+    name            = "StudentIndex"
+    hash_key        = "StudentID"
+    range_key       = "Timestamp"
+    projection_type = "ALL"
+  }
+
   tags = {
     Environment = var.environment
     Project     = var.project_name
--- dynamodb_students.tf
+++ dynamodb_students.tf
@@ -1,52 +1,24 @@
 resource "aws_dynamodb_table" "students" {
-  name           = "${var.project_name}-Students-${var.environment}"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "StudentID"
+  name         = "${var.project_name}-Students-${var.environment}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "StudentID"
+
   attribute {
     name = "StudentID"
     type = "S"
   }
+
   attribute {
     name = "Email"
     type = "S"
   }
-  attribute {
-    name = "Name"
-    type = "S"
-  }
 
-  attribute {
-    name = "CreatedAt"
-    type = "S"
-  }
-
-  attribute {
-    name = "SessionExpiresAt"
-    type = "S"
-  }
-
-  attribute {
-    name = "TopicsWeak"
-    type = "S"
-  }
-
-  attribute {
-    name = "TotalQuizzes"
-    type = "N"
-  }
-
-  attribute {
-    name = "AvgScore"
-    type = "N"
-  }
   global_secondary_index {
     name            = "EmailIndex"
+    hash_key        = "Email"
     projection_type = "ALL"
-    key_schema {
-      attribute_name = "Email"
-      key_type       = "HASH"  
-    }
   }
+
   tags = {
     Environment = var.environment
     Project     = var.project_name
