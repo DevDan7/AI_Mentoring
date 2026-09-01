d5dcf5dffbe3cc1070426f1b598458fe35d2c211
Daniel Villegas <danielsvillegas17@gmail.com>
2026-09-01 12:16:55 -0300

    fix: add QUIZZES_TABLE env var to student API Lambda
    - Required for get_quiz_history to query quizzes table

--- lambda_student_api.tf
+++ lambda_student_api.tf
@@ -21,6 +21,7 @@ resource "aws_lambda_function" "student_api" {
       COGNITO_CLIENT_ID    = aws_cognito_user_pool_client.student_app.id
       STUDENTS_TABLE       = aws_dynamodb_table.students.name
       COHORTS_TABLE        = aws_dynamodb_table.cohorts.name
+      QUIZZES_TABLE        = aws_dynamodb_table.quizzes.name
     }
   }
 
